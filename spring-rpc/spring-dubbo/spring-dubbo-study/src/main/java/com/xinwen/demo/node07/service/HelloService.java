0dd49c90eb7dd14e71f00f818a9d7b71366b4b82
shenlx <597213071@qq.com>
2025-03-14 21:20:27 +0800

    JDK17的新特性

---
+++ spring-rpc/spring-dubbo/spring-dubbo-study/src/main/java/com/xinwen/demo/node07/service/HelloService.java
@@ -0,0 +1,10 @@
+package com.xinwen.demo.node07.service;
+
+/**
+ * @author shenlx
+ * @description 服务接口:在RPC中，定义生产者和消费者有一个共同的服务接口API
+ * @date 2025/3/14 21:05
+ */
+public interface HelloService {
+    String sayHello(String someBody);
+}
