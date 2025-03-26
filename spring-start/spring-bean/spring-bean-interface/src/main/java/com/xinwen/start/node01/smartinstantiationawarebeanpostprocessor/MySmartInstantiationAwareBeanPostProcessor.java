9b2a6aa705ff68d17a7ebc50eb7595e11f85fff4
shenlx <597213071@qq.com>
2025-03-26 10:53:41 +0800

    Springboot的常用扩展接口

---
+++ spring-start/spring-bean/spring-bean-interface/src/main/java/com/xinwen/start/node01/smartinstantiationawarebeanpostprocessor/MySmartInstantiationAwareBeanPostProcessor.java
@@ -0,0 +1,11 @@
+package com.xinwen.start.node01.smartinstantiationawarebeanpostprocessor;
+
+import org.springframework.beans.factory.config.SmartInstantiationAwareBeanPostProcessor;
+
+/**
+ * @author shenlx
+ * @description
+ * @date 2025/3/25 15:29
+ */
+public class MySmartInstantiationAwareBeanPostProcessor implements SmartInstantiationAwareBeanPostProcessor {
+}
